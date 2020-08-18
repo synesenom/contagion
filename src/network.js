631eba9adf42b15148109cf6116c676906224f8a
synesenom <enys.mones@gmail.com>
2020-08-18 18:36:19 +0200

    :sparkles: load functionality added to Network

--- src/network.js
+++ src/network.js
@@ -1,3 +1,5 @@
+const { nest } = require('d3-collection')
+const { readCSV } = require('./io')
 const compose = require('./compose')
 const Logger = require('./components/logger')
 const Serializable = require('./components/serializable')
@@ -21,10 +23,42 @@ module.exports = () => {
     TimeStep,
     Transition
   ], {
-    config: {
-      k: 1
-    }
+    config: {},
+    graph: {}
   })
+
+  api.load = async path => {
+    _.log.i(`Loading network from: ${path}`)
+
+    // Load and map data.
+    let data = await readCSV(path).catch(_.log.e)
+    data = data.map(d => ({
+      ts: +d['ts'],
+      n1: +d['n1'],
+      n2: +d['n2']
+    }))
+    _.log.i(`  number of links: ${data.length}`)
+
+    // Extract nodes.
+    _.graph.nodes = [...new Set(data.map(d => [d.n1, d.n2]).flat())]
+      .sort((a, b) => a - b)
+    _.log.i(`  number of nodes: ${_.graph.nodes.length}`)
+
+    // Extract links.
+    _.graph.links = new Map(nest()
+      .key(d => d.ts)
+      .entries(data)
+      .map(d => [
+        +d.key,
+        d.values.map(v => ({
+          n1: Math.min(v.n1, v.n2),
+          n2: Math.max(v.n1, v.n2)
+        }))
+      ]))
+    _.log.i(`  number of bins:  ${_.graph.links.size}`)
+
+    return api
+  }
 
   return api
 }
--- test/network.js
+++ test/network.js
@@ -1,12 +1,13 @@
 const { assert } = require('chai')
 const { describe, it } = require('mocha')
+const stdout = require('test-console').stdout
 const Network = require('../src/network')
 
 
 describe('network', () => {
   describe('.deserialize', () => {
     it('should parse serialized dump correctly', () => {
-      const dump = '{"k":10,"states":{"a":"foo"},"dt":123,"transitions":[{"b":"bar"}]}'
+      const dump = '{"states":{"a":"foo"},"dt":123,"transitions":[{"b":"bar"}]}'
       assert.equal(Network().deserialize(dump).serialize(), dump)
     })
   })
@@ -15,8 +16,29 @@ describe('network', () => {
     it('should update the time resolution', () => {
       assert.equal(
         Network().dt(123).serialize(),
-        '{"k":1,"states":{},"dt":123,"transitions":[]}'
+        '{"states":{},"dt":123,"transitions":[]}'
       )
+    })
+  })
+
+  describe('.load', () => {
+    it('should print out correct load messages', async () => {
+      const path = './test/data/network.load.csv'
+      const inspect = stdout.inspect();
+      await Network().load(path)
+      inspect.restore();
+      assert.deepEqual(inspect.output, [
+        `\u001b[37mINFO [00:00:00]: Loading network from: ${path}\u001b[39m\n`,
+        `\u001b[37mINFO [00:00:00]:   number of links: 10\u001b[39m\n`,
+        `\u001b[37mINFO [00:00:00]:   number of nodes: 4\u001b[39m\n`,
+        `\u001b[37mINFO [00:00:00]:   number of bins:  5\u001b[39m\n`
+      ]);
+    })
+  })
+
+  describe('.serialize', () => {
+    it('should dump the network config as a string', () => {
+      assert.deepEqual(Network().serialize(), '{"states":{},"dt":1,"transitions":[]}')
     })
   })
 
@@ -24,7 +46,7 @@ describe('network', () => {
     it('should add states to the config states', () => {
       assert.equal(
         Network().state('a').state('b').state('c').serialize(),
-        '{"k":1,"states":{"a":2,"b":3,"c":5},"dt":1,"transitions":[]}'
+        '{"states":{"a":2,"b":3,"c":5},"dt":1,"transitions":[]}'
       )
     })
   })
@@ -36,14 +58,8 @@ describe('network', () => {
           .transition('infection', [['s', 'i'], ['i', 'i']], 0.1)
           .transition('recovery', [['i'], ['r']], 2, (p, dt) => dt / p)
           .serialize(),
-      '{"k":1,"states":{},"dt":1,"transitions":[{"name":"infection","before":["s","i"],"after":["i","i"],"p":0.1},{"name":"recovery","before":["i"],"after":["r"],"p":0.5}]}'
+        '{"states":{},"dt":1,"transitions":[{"name":"infection","before":["s","i"],"after":["i","i"],"p":0.1},{"name":"recovery","before":["i"],"after":["r"],"p":0.5}]}'
       )
-    })
-  })
-
-  describe('.serialize', () => {
-    it('should dump the network config as a string', () => {
-      assert.deepEqual(Network().serialize(), '{"k":1,"states":{},"dt":1,"transitions":[]}')
     })
   })
 })
